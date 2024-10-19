528c1e79b5a0ef90ac56ebbc3d57a174084ae849
Kyouji <shirokung003@gmail.com>
2024-10-19 17:42:08 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { LoginComponent } from './admin/login/login.component';
 import { ProductComponent } from './product/productcomponent';
+import { FilterComponent } from './filter/filter.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, 
   { path: 'home', component: HomeComponent }, 
-  { path: 'product', component: ProductComponent }, 
+  { path: 'product', component: ProductComponent },
+  { path: 'filter', component: FilterComponent },
   { path: 'about-us', component: AboutUsComponent }, 
   { path: 'admin/admin-login', component: LoginComponent },,
   { path: 'admin', redirectTo: 'admin/admin-login', pathMatch: 'full' }
